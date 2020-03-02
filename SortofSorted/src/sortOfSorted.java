94027593652d368e0ed234584a2b5ba2505445d0
dmchavira1 <Pizza101@>
2020-03-02 14:48:18 -0700

    Started lab

---
+++ SortofSorted/src/sortOfSorted.java
@@ -0,0 +1,12 @@
+public class sortOfSorted {
+    public int m(int[] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length - 1; i++){
+            for( int k = i + 1; k < arr.length; k++){
+                
+            }
+            sum+=arr[i];
+        }
+        return sum;
+    }
+}
